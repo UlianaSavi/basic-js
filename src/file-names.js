46148c966aae82a26f5afe67070721ffdfda48f8
UlianaSavi <uljasha.sav@gmail.com>
2022-10-17 23:55:13 +0600

    feat: add file names solution

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,6 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const {
+  NotImplementedError
+} = require('../extensions/index.js');
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,9 +17,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  let result = [];
+  names.forEach((el) => {
+    if (result.includes(el)) {
+      let i = 1;
+      while (result.includes(`${el}(${i})`)) {
+        i++;
+      }
+      result.push(`${el}(${i})`);
+    } else {
+      result.push(el);
+    }
+  });
+  return result;
 }
 
 module.exports = {
